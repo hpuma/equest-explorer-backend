f4d5453399734bd80e44d867c3e9101ba8e373db
hpuma <pumahenry@live.com>
2022-12-14 12:17:45 -0500

    Added global history resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsModule } from './news/news.module';
 import { QuoteModule } from './quote/quote.module';
+import { HistoryModule } from './history/history.module';
 
 @Module({
-  imports: [NewsModule, QuoteModule],
+  imports: [NewsModule, QuoteModule, HistoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
