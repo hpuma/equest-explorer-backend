bc65e1c5868ec80d464ecfa0325ec3e57b409651
hpuma <pumahenry@live.com>
2022-12-14 12:16:39 -0500

    Added global news source

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [],
+  imports: [NewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
