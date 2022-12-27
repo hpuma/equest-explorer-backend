6d644bf74f17bc0abbd9f7cebba6d38f73707b19
Henry Puma <37905046+hpuma@users.noreply.github.com>
2022-12-27 13:24:10 -0500

    Embedd news api to news endpoint

---
+++ src/news/api/news-api.service.ts
@@ -0,0 +1,17 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ConfigType } from '@nestjs/config';
+import apisConfig from '../../apis.config';
+
+@Injectable()
+export class NewsApiService {
+  private apiKey: string;
+  constructor(
+    private readonly httpService: HttpService,
+    @Inject(apisConfig.KEY)
+    newsApiConfig: ConfigType<typeof apisConfig>,
+  ) {
+    httpService.axiosRef.defaults.baseURL = newsApiConfig.newsApiBaseURL;
+    this.apiKey = newsApiConfig.newsApiKey;
+  }
+}
