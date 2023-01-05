bda4ad4fd29161cd225ee5175b13a9e7060ca39b
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-01-05 07:46:35 -0500

    Updated config for httpModule

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { NewsService } from './news.service';
 import { NewsController } from './news.controller';
 import { NewsApiService } from './api/news-api.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        baseURL: configService.get('NEWS_API_BASE_URL'),
+        headers: {
+          'X-Api-Key': configService.get('NEWS_API_KEY'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [NewsController],
   providers: [NewsService, NewsApiService],
 })
