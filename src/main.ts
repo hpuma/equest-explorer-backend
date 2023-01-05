6ce7e91b3050d35220462dbb2e3afddfcaab0c3f
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-01-05 00:57:21 -0500

    Added alias config to main file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'module-alias/register';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
