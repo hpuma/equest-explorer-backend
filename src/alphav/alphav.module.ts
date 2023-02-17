1a810d581574065685d5133713522d447ceacfb7
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-02-17 10:45:29 -0500

    Updated alphav module

--- src/alphav/alphav.module.ts
+++ src/alphav/alphav.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 import { AlphavService } from './alphav.service';
 import { AlphavController } from './alphav.controller';
 import { AlphavApiService } from './api/alphav-api.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 @Module({
+  imports: [
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        baseURL: configService
+          .get('ALPHAV_BASE_URL')
+          .concat(`?apikey=${configService.get('ALPHAV_API_KEY')}`),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AlphavController],
   providers: [AlphavService, AlphavApiService],
 })
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsModule } from './news/news.module';
 import { ConfigModule } from '@nestjs/config';
+import { AlphavModule } from './alphav/alphav.module';
 
 @Module({
-  imports: [NewsModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [NewsModule, ConfigModule.forRoot({ isGlobal: true }), AlphavModule],
   controllers: [AppController],
   providers: [AppService],
 })
