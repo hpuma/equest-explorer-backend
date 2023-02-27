f7c1ae22cddccf9e2e11c42ad84b6dc2d66ebfb1
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-02-27 00:11:09 -0500

    Remove old market aux service and controller providers from global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,17 @@ import { AppService } from './app.service';
 import { NewsModule } from './news/news.module';
 import { ConfigModule } from '@nestjs/config';
 import { AlphavModule } from './alphav/alphav.module';
-import { MarketauxController } from './marketaux/marketaux.controller';
-import { MarketauxService } from './marketaux/marketaux.service';
+
 import { MarketauxModule } from './marketaux/marketaux.module';
 
 @Module({
-  imports: [NewsModule, ConfigModule.forRoot({ isGlobal: true }), AlphavModule, MarketauxModule],
-  controllers: [AppController, MarketauxController],
-  providers: [AppService, MarketauxService],
+  imports: [
+    NewsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AlphavModule,
+    MarketauxModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
