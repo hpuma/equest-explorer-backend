a38daa20944da8a45b9ba54ec6a85189784a63aa
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-03-10 01:41:17 -0500

    Added base unit tests for alphav, need to establish testing pattern. Saving progress

--- src/alphav/alphav.controller.spec.ts
+++ src/alphav/alphav.controller.spec.ts
@@ -1,20 +1,59 @@
+import { GlobalModule } from '@global/global.module';
+import { GlobalValidator } from '@global/validation/global-validator.class';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AlphavController } from './alphav.controller';
 import { AlphavService } from './alphav.service';
+import { GetResponseDto } from './api/dto/get-response.dto';
+import { IntradayQueryDto } from './dto/intraday-query.dto';
 
 describe('AlphavController', () => {
   let controller: AlphavController;
+  let service: AlphavService;
+  let response;
+  let globalValidator: GlobalValidator;
+  const res: any = {
+    send: jest.fn(),
+    json: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [GlobalModule],
       controllers: [AlphavController],
-      providers: [AlphavService],
+      providers: [
+        {
+          provide: AlphavService,
+          useValue: { getIntraday: jest.fn() },
+        },
+      ],
     }).compile();
 
     controller = module.get<AlphavController>(AlphavController);
+    service = module.get<AlphavService>(AlphavService);
+    globalValidator = module.get<GlobalValidator>(GlobalValidator);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  describe('Should return a response', () => {
+    it('Should return a successful response', async () => {
+      jest
+        .spyOn(service, 'getIntraday')
+        .mockResolvedValue({ intradayParams: 'WIP' } as GetResponseDto);
+
+      jest
+        .spyOn(globalValidator, 'validate')
+        .mockResolvedValue({ validateParams: 'WIP' } as GetResponseDto);
+
+      response = await controller.intraday(
+        { ticker: 'SPY' } as IntradayQueryDto,
+        res,
+      );
+
+      expect(res.json).toBeCalledWith({ validateParams: 'WIP' });
+      expect(response).toEqual({ validateParams: 'WIP' });
+    });
+    it.skip('Should return an unsuccessful response', async () => {});
   });
 });
