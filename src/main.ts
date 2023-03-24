8857b8a9ab9e8f89d97b0abb83ea29d4a72de17a
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-03-24 02:44:27 -0400

    Apply swagger to main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'module-alias/register';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle('EQUEST Backend API')
+    .setDescription(
+      'Equest endpoint documentation for integrations: alphav, marketaux, news',
+    )
+    .setVersion('0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3001);
 }
