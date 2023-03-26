aa5bab03ab9e5e57afda680a0ec2f6d573027425
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-03-26 19:10:01 -0400

    Added documentation for alphavantage intraday query parameters

--- src/alphav/dto/intraday-query.dto.ts
+++ src/alphav/dto/intraday-query.dto.ts
@@ -12,9 +12,20 @@ export class IntradayQueryDto {
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    description:
+      'Time interval between two consecutive data points in the time series.',
+    enum: ['1min', '5min', '15min', '30min', '60min'],
+  })
   interval?: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    description: `compact returns only the latest 100 data points in the intraday time series; 
+      full returns the full-length intraday time series;
+      The "compact" option is recommended if you would like to reduce the data size of each API call. `,
+    enum: ['compact', 'full'],
+  })
   datasize?: string;
 }
