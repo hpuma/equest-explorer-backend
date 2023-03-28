648331ce4269583c619c012db559ed27379919c2
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-03-28 01:07:04 -0400

    Add documentation for IntradayResponseDto

--- src/alphav/dto/intraday-response.dto.ts
+++ src/alphav/dto/intraday-response.dto.ts
@@ -1,43 +1,95 @@
-import { IsString, ValidateNested, IsArray } from 'class-validator';
+import { IsString, ValidateNested, IsArray, IsNumber } from 'class-validator';
 import { Type } from 'class-transformer';
 import { Format } from '@alphav/helpers/format.class';
 import { GetResponseDto } from '@alphav/api/dto/get-response.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ChartTimeSeries {
   @IsString()
+  @ApiProperty({
+    description: 'timestamp of stock data',
+    example: '8:00:00 PM',
+    type: Date,
+  })
   x!: Date;
 
+  @IsNumber({}, { each: true })
+  @ApiProperty({
+    description: 'array containing open, high, low, current',
+    example: [397.02, 397.03, 396.98, 397.01],
+    type: [Number],
+  })
   y!: number[];
 }
 
 export class MetaData {
   @IsString()
+  @ApiProperty({
+    description:
+      'basic description of timeseries data such as interval, O, H, L, C, V',
+    example: 'Intraday (1min) open, high, low, close prices and volume',
+  })
   info: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'stock ticker',
+    example: 'IBM',
+  })
   symbol: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'time stamp of last time data was updated',
+    example: '2023-03-27 19:40:00',
+  })
   lastupdated: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'interval value from request query param',
+    example: '1min',
+  })
   interval: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'datasize value from request query param',
+    example: 'Compact',
+  })
   size: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'data source timezone',
+    example: 'US/Eastern',
+  })
   timezone: string;
 }
 
 export class Timeseries {
-  [key: string]: {
-    open: string;
-    high: string;
-    low: string;
-    close: string;
-    volume: string;
-  };
+  @IsString()
+  @ApiProperty({ description: 'interval open', example: '117' })
+  open: string;
+
+  @IsString()
+  @ApiProperty({ description: 'interval high', example: '120' })
+  high: string;
+
+  @IsString()
+  @ApiProperty({ description: 'interval low', example: '115' })
+  low: string;
+
+  @IsString()
+  @ApiProperty({ description: 'interval close', example: '119' })
+  close: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'interval total number of shares traded',
+    example: '74010408',
+  })
+  volume: string;
 }
 
 export class IntradayResponseDto {
@@ -60,17 +112,36 @@ export class IntradayResponseDto {
 
   @ValidateNested()
   @Type(() => MetaData)
+  @ApiProperty({
+    description: 'intraday metadata',
+  })
   metadata: MetaData;
 
   @IsString()
+  @ApiProperty({
+    description: 'aggregate time timeseries field',
+    example: '1min',
+  })
   interval: string;
 
   @ValidateNested()
   @Type(() => Timeseries)
-  timeseries: Timeseries;
+  @ApiProperty({
+    description: 'list of timeseries within interval',
+    type: Timeseries,
+    isArray: true,
+    additionalProperties: { type: 'string' },
+  })
+  timeseries: Record<string, Timeseries>;
 
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => ChartTimeSeries)
+  @ApiProperty({
+    description:
+      'list of chartTimeSeries within interval, NOTE: need to make optional',
+    type: ChartTimeSeries,
+    isArray: true,
+  })
   chartTimeSeries: ChartTimeSeries[];
 }
