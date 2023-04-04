111ada3de220a0e4a47786d7aba5bc183189125f
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-04-04 15:32:42 -0400

    Simplified config validator

--- src/global/env/validate.ts
+++ src/global/env/validate.ts
@@ -2,13 +2,18 @@ import { Expose, plainToInstance } from 'class-transformer';
 import { IsBoolean, IsNotEmpty, IsString, validateSync } from 'class-validator';
 import { ConsoleLogger } from '@nestjs/common';
 
-const ApiBaseUrls = {
-  NEWS_BASE_URL: 'https://newsapi.org/v2',
-  ALPHAV_BASE_URL: 'https://www.alphavantage.co/query',
-  MARKETAUX_BASE_URL: 'https://api.marketaux.com/v1/',
-};
+class ApiBaseUrls {
+  @IsString()
+  NEWS_BASE_URL = 'https://newsapi.org/v2';
 
-class EnvVariables {
+  @IsString()
+  ALPHAV_BASE_URL = 'https://www.alphavantage.co/query';
+
+  @IsString()
+  MARKETAUX_BASE_URL = 'https://api.marketaux.com/v1/';
+}
+
+class EnvVariables extends ApiBaseUrls {
   @IsString()
   @IsNotEmpty()
   @Expose()
@@ -33,27 +38,10 @@ class EnvVariables {
   @IsNotEmpty()
   @Expose()
   MARKETAUX_API_KEY: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @Expose()
-  NEWS_BASE_URL: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @Expose()
-  ALPHAV_BASE_URL: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @Expose()
-  MARKETAUX_BASE_URL: string;
 }
 
 export function validate(config: Record<string, unknown>) {
-  const configWithBaseUrls = { ...config, ...ApiBaseUrls };
-
-  const validatedConfig = plainToInstance(EnvVariables, configWithBaseUrls, {
+  const validatedConfig = plainToInstance(EnvVariables, config, {
     enableImplicitConversion: true,
     excludeExtraneousValues: true,
   });
