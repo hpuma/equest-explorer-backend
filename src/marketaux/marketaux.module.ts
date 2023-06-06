6e4c56f718862ae4360d8c59d568241412e03fec
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-06-06 03:58:23 -0400

    Set up marketaux module

--- src/marketaux/marketaux.module.ts
+++ src/marketaux/marketaux.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { MarketauxService } from './marketaux.service';
 import { MarketauxController } from './marketaux.controller';
 import { MarketauxApiService } from './api/marketaux-api.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { GlobalModule } from '@global/global.module';
 
 @Module({
+  imports: [
+    GlobalModule,
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        baseURL: configService.get('MARKETAUX_BASE_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [MarketauxController],
-  providers: [MarketauxService, MarketauxApiService],
+  providers: [MarketauxService, MarketauxApiService, ConfigService],
 })
 export class MarketauxModule {}
