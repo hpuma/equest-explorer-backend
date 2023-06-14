41861bda9616df6ee8c25fb358654e01f1d4ef11
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-06-13 21:16:37 -0400

    Removed NewsRecordResponesDto

--- src/equest/dto/newsrecord/newsrecord-response.dto.ts
+++
@@ -1,8 +0,0 @@
-import { NewsResource } from '@global/newsresource.class';
-
-export class NewsRecordResponseDto extends NewsResource {
-  constructor(data: NewsRecordResponseDto) {
-    super(data);
-    if (data) Object.assign(this, data);
-  }
-}
