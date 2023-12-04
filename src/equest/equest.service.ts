4d7e5258e5c2deea4c48f0ae1a9af6a95dea620d
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-12-04 03:04:32 -0500

    Create equest methods for tickerQuote

--- src/equest/equest.service.ts
+++ src/equest/equest.service.ts
@@ -3,6 +3,7 @@ import { Injectable, Inject } from '@nestjs/common';
 import { ApiKey, API_KEY } from '@database/models/apikey.model';
 import { NewsRecord, NEWS_RECORD } from '@database/models/newsrecord.model';
 import { TickerValue, TICKER_VALUE } from '@database/models/tickervalue.model';
+import { TickerQuote, TICKER_QUOTE } from '@database/models/tickerquote.model';
 import { v4 as uuidv4 } from 'uuid';
 import { Article } from '@global/newsresource.class';
 
@@ -15,6 +16,8 @@ export class EquestService {
     private newsRecord: Model<NewsRecord>,
     @Inject(TICKER_VALUE)
     private tickerValue: Model<TickerValue>,
+    @Inject(TICKER_QUOTE)
+    private tickerQuote: Model<TickerQuote>,
   ) {}
   async createNewsRecords(articles: Article[]) {
     const { acknowledged = false, insertedCount = 0 } =
@@ -92,5 +95,23 @@ export class EquestService {
     });
 
     return { key };
+  }
+  async createTickerQuote(data: any): Promise<TickerQuote> {
+    const timeThreshold = new Date(); // Tiume now
+    timeThreshold.setHours(timeThreshold.getHours() - 1); // Set time an hour back
+
+    return (
+      (await this.tickerQuote
+        .findOne({
+          symbol: data.symbol,
+          createdAt: { $gte: timeThreshold },
+        })
+        .lean()) ?? (await this.tickerQuote.create(data)).toJSON()
+    );
+  }
+  async getTickerQuote(symbol: string): Promise<TickerQuote> {
+    return await this.tickerQuote
+      .findOne({ symbol }, {}, { sort: { createdAt: -1 } })
+      .lean();
   }
 }
