f0deb5e58d98d5fe595e784f5a29bbcfdd625913
Henry Puma <37905046+hpuma@users.noreply.github.com>
2024-01-28 20:49:08 -0500

    Removed dead import

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import { AppModule } from './app.module';
 import { AuthGuard, AuthGuardService } from 'auth';
 import * as express from 'express';
 
-import 'module-alias/register';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(express.json({ limit: '10mb' }));
@@ -27,6 +25,7 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.enableCors({ origin: true, credentials: true });
   await app.listen(3001);
 }
 bootstrap();
